8920f40cbfd6e1ae0ca1ee5815a25cb0a71f3741
alaaalasoppp1-maker <alaa.alasoppp1@gmail.com>
2026-07-17 00:47:38 +0300

    Add files via upload

---
+++ src/shared/protocol.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const COMMANDS = Object.freeze({
+  HOME: "home",
+  PATIENT: "patient",
+  IMAGE: "image",
+  GIF: "gif",
+  VIDEO: "video",
+  PDF: "pdf",
+  BLACK: "black",
+  HIDE: "hide",
+  TRANSFORM: "transform",
+  RESET_VIEW: "reset_view",
+  PING: "ping"
+});
+
+module.exports = {COMMANDS};
